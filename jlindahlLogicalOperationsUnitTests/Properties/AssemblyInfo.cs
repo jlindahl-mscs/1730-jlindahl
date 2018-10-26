7755bff4c052d9e1f2a9c6b32bc4ceabe1167a8c
jlindahl-mscs <Joslyn.Lindahl@my.southeastmn.edu>
2018-10-26 15:09:44 -0500

    Completed Ex 2E

---
+++ jlindahlLogicalOperationsUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jlindahlLogicalOperationsUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("jlindahlLogicalOperationsUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ba21584-d9f9-4d92-9eaf-b4d25be9e8ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
