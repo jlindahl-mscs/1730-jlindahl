483eb90dd3fe0312aa3dfbb21c7c93fca4039dbb
jlindahl-mscs <Joslyn.Lindahl@my.southeastmn.edu>
2018-11-30 12:44:10 -0600

    Hi Mark, I have updated Visual Studio and done everything I can think of but the unit Tests for the loops just won't give a pass or fail, they just spin endlessly. So I am pushing and committing to github like you asked.

---
+++ jlindahl3A1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jlindahl3A1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox0.Text = Ex3aLoops.Calc0(inputTextBox0A.Text, inputTextBox0B.Text, inputTextBox0C.Text);
+            resultTextBox1.Text = Ex3aLoops.Calc1(inputTextBox1A.Text, inputTextBox1B.Text, inputTextBox1C.Text) ;
+            resultTextBox2.Text = Ex3aLoops.Calc2(inputTextBox2A.Text, inputTextBox2B.Text, inputTextBox2C.Text);
+        }
+    }
+}
---
+++ jlindahl3AUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jlindahl3AUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("jlindahl3AUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1a77d5c-21a6-4059-96c5-6c7530141b2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
