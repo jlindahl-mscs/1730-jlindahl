974103c4d9b794df0e7a56360d841804342edf9c
jlindahl-mscs <Joslyn.Lindahl@my.southeastmn.edu>
2018-12-05 22:01:33 -0600

    Completed 3a

--- jlindahl3A1/Ex3aLoops.cs
+++ jlindahl3A1/Ex3aLoops.cs
@@ -29,7 +29,7 @@ namespace jlindahl3A1
                 double monthlyPayment = Double.Parse(strMonthlyPayment);
                 double monthlyInterestRate = Double.Parse(strMonthlyInterestRate);
                 int i = 1;
-                while (1 <= months)
+                while (i <= months)
                 {
                     futureValue = (futureValue + monthlyPayment) * (1 + monthlyInterestRate);
                     i++;
@@ -56,7 +56,7 @@ namespace jlindahl3A1
                     futureValue = (futureValue + monthlyPayment) * (1 + monthlyInterestRate);
                     i++;
                 }
-                while (1 <= months);
+                while (i <= months);
             }
             catch
             {
@@ -86,7 +86,25 @@ namespace jlindahl3A1
             }
         public static string Calc3(string strStart, string strEnd, string strIncrement)
         {
+            //Count, do
             string result = "";
+            try
+            {
+                int increment = Int32.Parse(strIncrement);
+                int end = Int32.Parse(strEnd);
+                int start = Int32.Parse(strStart);
+                int i = start;
+                if (increment <= 0) throw new Exception();
+                while (i <= end)
+                {
+                    result = result + start + (1 * increment);
+                    i++;
+                }
+            }
+            catch
+            {
+                return "Invalid input";
+            }
 
             return result;
         }
@@ -94,35 +112,109 @@ namespace jlindahl3A1
         public static string Calc4(string strStart, string strEnd, string strIncrement)
         {
             string result = "";
-
+            try
+            {
+                int increment = Int32.Parse(strIncrement);
+                int end = Int32.Parse(strEnd);
+                int start = Int32.Parse(strStart);
+                int i = 1;
+                if (increment <= 0) throw new Exception();
+                do
+                {
+                    result = result += start += increment;
+                    i++;
+                }
+                while (i <= end);
+            }
+            catch
+            {
+                return "Invalid input";
+            }
             return result;
         }
 
         public static string Calc5(string strStart, string strEnd, string strIncrement)
         {
             string result = "";
-
+            try
+            {
+                int increment = Int32.Parse(strIncrement);
+                int end = Int32.Parse(strEnd);
+                int start = Int32.Parse(strStart);
+                int i = 1;
+                if (increment <= 0) throw new Exception();
+                if (start >= end) throw new Exception();
+                for (i = 1; i < start;)
+                {
+                    result = result += start * increment;
+                }
+            }
+            catch
+            {
+                return "Invalid input";
+            }
             return result;
         }
 
         public static string Calc6(string strNumbers, string strCount)
         {
             string result = "";
+            int startIndex = 0;
+            try
+            {
+                int count = Int32.Parse(strCount);
+                
+                while (count <= startIndex)
+                {
+                    int endIndex = strNumbers.IndexOf(' ', startIndex);
+                    string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                    int number = Int32.Parse(strNumber);
 
+                    startIndex = endIndex + 1;
+
+                    count += number;
+                    count++;
+                }
+
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
             return result;
         }
 
         public static string Calc7(string strNumbers, string strCount)
         {
             string result = "";
+            int startIndex = 0;
+            try
+            {
+                int endIndex = strNumbers.IndexOf(' ', startIndex);
+                string strNumber = strNumbers.Substring(startIndex, endIndex - startIndex);
+                int number = Int32.Parse(strNumber);
 
+                startIndex = endIndex + 1;
+            }
+            catch
+            {
+                return "Invalid input";
+            }
             return result;
         }
 
         public static string Calc8(string strNumbers, string strCount)
         {
             string result = "";
+            int startIndex = 0;
+            try
+            {
 
+            }
+            catch
+            {
+                return "Invalid input";
+            }
             return result;
         }
 
--- jlindahl3A1/MainWindow.xaml.cs
+++ jlindahl3A1/MainWindow.xaml.cs
@@ -28,8 +28,14 @@ namespace jlindahl3A1
         private void calcButton_Click(object sender, RoutedEventArgs e)
         {
             resultTextBox0.Text = Ex3aLoops.Calc0(inputTextBox0A.Text, inputTextBox0B.Text, inputTextBox0C.Text);
-            resultTextBox1.Text = Ex3aLoops.Calc1(inputTextBox1A.Text, inputTextBox1B.Text, inputTextBox1C.Text) ;
+            resultTextBox1.Text = Ex3aLoops.Calc1(inputTextBox1A.Text, inputTextBox1B.Text, inputTextBox1C.Text);
             resultTextBox2.Text = Ex3aLoops.Calc2(inputTextBox2A.Text, inputTextBox2B.Text, inputTextBox2C.Text);
+            resultTextBox3.Text = Ex3aLoops.Calc3(inputTextBox3A.Text, inputTextBox3B.Text, inputTextBox3C.Text);
+            resultTextBox4.Text = Ex3aLoops.Calc4(inputTextBox4A.Text, inputTextBox4B.Text, inputTextBox4C.Text);
+            resultTextBox5.Text = Ex3aLoops.Calc5(inputTextBox5A.Text, inputTextBox5B.Text, inputTextBox5C.Text);
+            resultTextBox6.Text = Ex3aLoops.Calc6(inputTextBox6A.Text, inputTextBox6B.Text);
+            resultTextBox7.Text = Ex3aLoops.Calc7(inputTextBox7A.Text, inputTextBox7B.Text);
+            resultTextBox8.Text = Ex3aLoops.Calc8(inputTextBox8A.Text, inputTextBox8B.Text);
         }
     }
 }
